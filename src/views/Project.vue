eae846550655cd5b7c32614279c9d4f19b37e67c
sasaken <sasakendodt3@gmail.com>
2020-05-03 21:01:46 +0900

    GitHub・Appへのリンク設置

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -2,11 +2,11 @@
   <section class="project">
     <div class="container">
       <div class="text">
-        <span class="fas fa-database icon"></span>
+        <p><img src="../assets/VRD.png" class="logo"></p>
         <h2>V_Research_Database</h2>
-        <p>リサーチソースをタグ付けして管理するアプリケーション</p>
-        <p>使用技術：Ruby, Ruby on Rails </p>
-        <a href="https://github.com/sasakenken33/V_Research_Database" target="_blank">Jump to GitHub...
+        <p>リサーチソースをタグ付管理するアプリケーション</p>
+        <p>使用技術：Ruby, Ruby on Rails<a href="https://github.com/sasakenken33/VRD" target="_blank" class="github"><span class="fab fa-github"></span></a></p>
+        <a href="https://v-research-database.herokuapp.com/" target="_blank">Jump to App
         <span class="fa fa-chevron-right"></span>
         </a>
       </div>
@@ -14,8 +14,8 @@
         <p><img src="../assets/ARCS_normal_cosme.png" class="logo"></p>
         <h2>ARCS_@cosme</h2>
         <p>@cosmeレビューを自動収集するスクレイピングツール(Ruby on Rails版)</p>
-        <p>使用技術：Ruby, Ruby on Rails </p>
-        <a href="https://github.com/sasakenken33/ARCS_atcosme" target="_blank">Jump to GitHub...
+        <p>使用技術：Ruby, Ruby on Rails<a href="https://github.com/sasakenken33/ARCS_atcosme" target="_blank" class="github"><span class="fab fa-github"></span></a></p>
+        <a href="https://arcs-atcosme.herokuapp.com/" target="_blank">Jump to App
         <span class="fa fa-chevron-right"></span>
         </a>
       </div>
@@ -23,8 +23,8 @@
         <p><img src="../assets/ARCS_normal_cosme.png" class="logo"></p>
         <h2>ARCS_@cosme</h2>
         <p>@cosmeレビューを自動収集するスクレイピングツール(Flask版)</p>
-        <p>使用技術：Python, Flask </p>
-        <a href="https://github.com/sasakenken33/ARCS_atcosme" target="_blank">Jump to GitHub...
+        <p>使用技術：Python, Flask<a href="https://github.com/sasakenken33/ARCS_at_cosme" target="_blank" class="github"><span class="fab fa-github"></span></a></p>
+        <a href="https://arcs-at-cosme.herokuapp.com/" target="_blank">Jump to App
         <span class="fa fa-chevron-right"></span>
         </a>
       </div>
@@ -32,8 +32,8 @@
         <p><img src="../assets/Meiseki.png" class="logo"></p>
         <h2>MEISEKI</h2>
         <p>文学作品を形態素解析し、傾向値を算出するツール</p>
-        <p>使用技術：Python, Flask</p>
-        <a href="https://github.com/sasakenken33/morpheme_analyzer" target="_blank">Jump to GitHub...
+        <p>使用技術：Python, Flask<a href="https://github.com/sasakenken33/morpheme_analyzer" target="_blank" class="github"><span class="fab fa-github"></span></a></p>
+        <a href="https://meiseki.herokuapp.com/" target="_blank">Jump to App
         <span class="fa fa-chevron-right"></span>
         </a>
       </div>
@@ -41,17 +41,14 @@
         <span class="fab fa-twitter icon"></span>
         <h2>Twitter Analyzer</h2>
         <p>Twitter投稿分析ツール</p>
-        <p>使用技術：Python, Flask, COTOHA API </p>
-        <a href="https://github.com/sasakenken33/twitter_analyzer" target="_blank">Jump to GitHub...
-        <span class="fa fa-chevron-right"></span>
-        </a>
+        <p>使用技術：Python, Flask, COTOHA API<a href="https://github.com/sasakenken33/twitter_analyzer" target="_blank" class="github"><span class="fab fa-github"></span></a></p>
       </div>
       <div class="text">
         <span class="fas fa-portrait icon"></span>
         <h2>My Portfolio Site</h2>
         <p>このポートフォリオサイト</p>
-        <p>使用技術：JavaScript, Vue.js </p>
-        <a href="https://github.com/sasakenken33/My_Profile" target="_blank">Jump to GitHub...
+        <p>使用技術：JavaScript, Vue.js<a href="https://github.com/sasakenken33/myportfolio" target="_blank" class="github"><span class="fab fa-github"></span></a></p>
+        <a href="https://sasakenken33.github.io/myportfolio/" target="_blank">Jump to App
         <span class="fa fa-chevron-right"></span>
         </a>
       </div>
@@ -59,19 +56,13 @@
         <span class="fas fa-mail-bulk icon"></span>
         <h2>Gmail自動整理ツール</h2>
         <p>Gmailを自動で整理してくれるツール</p>
-        <p>使用技術：JavaScript, Google App Script</p>
-        <a href="https://github.com/sasakenken33/Gmail_Manager3" target="_blank">Jump to GitHub...
-        <span class="fa fa-chevron-right"></span>
-        </a>
+        <p>使用技術：JavaScript, Google App Script<a href="https://github.com/sasakenken33/Gmail_Manager3" target="_blank" class="github"><span class="fab fa-github"></span></a></p>
       </div>
       <div class="text">
         <span class="far fa-calendar-alt icon"></span>
         <h2>Slack bot for Google-Calendar</h2>
         <p>Google-Calendarに登録した予定を毎朝お知らせしてくれるSlack bot</p>
-        <p>使用技術：JavaScript, Google App Script</p>
-        <a href="https://github.com/sasakenken33/G-Calendar_Manager" target="_blank">Jump to GitHub...
-        <span class="fa fa-chevron-right"></span>
-        </a>
+        <p>使用技術：JavaScript, Google App Script<a href="https://github.com/sasakenken33/G-Calendar_Manager" target="_blank" class="github"><span class="fab fa-github"></span></a></p>
       </div>
     </div>
   </section>
@@ -127,6 +118,10 @@
   }
   .logo {
     width: 15rem;
+  }
+  .github {
+    margin-left: 0.5rem;
+    font-size: 1.5rem;
   }
   
   @media (min-width: 768px) {
